32748fcf0280e24c3c680b6c96963ed646c0bbfd
Kumodatsu <dragonkumo@gmail.com>
2021-12-03 22:22:45 +0100

    Use dark magic to interweave parts 1 and 2

--- src/day-03/README.md
+++ src/day-03/README.md
@@ -6,7 +6,7 @@ Since a lot of this problem can be solved using fancy bit manipulation, C seems
 like an excellent choice to solve it. Also in the spirit of C, the program is as
 unreadable as possible, left completely undocumented and may or may not produce
 the intended result with your choice of compiler and environment. In particular,
-my compiler complains about undefined behavior on line 35 in `main.c`, but one
+my compiler complains about undefined behavior on line 24 in `main.c`, but one
 sometimes has to make sacrifices in order to remain concise.
 
 [1]: <https://adventofcode.com/2021/day/3>
--- src/day-03/main.c
+++ src/day-03/main.c
@@ -3,7 +3,8 @@
 #include <string.h>
 #include "line.h"
 
-#define MAGIC_NUMBER 64
+#define MAGIC_NUMBER        64
+#define UNHELPFUL_ERROR_MSG "invalid arguments"
 
 uint32_t evil_global;
 
@@ -11,51 +12,35 @@ int cmpui32asc(const void* a, const void* b) {
     return (*(uint32_t*) a & evil_global) - (*(uint32_t*) b & evil_global);
 }
 
-void part1(void) {
-    size_t l;
-    uint32_t g = 0, *o = 0, n = 0;
+int main(int argc, char** argv) {
+    uint32_t p = (argc == 2 ? strtoul(argv[1], 0, 10) : 0) - 1;
+    if (p > 1) { printf(UNHELPFUL_ERROR_MSG); return -1; }
+    size_t c = MAGIC_NUMBER, l = 0;
+    uint32_t *a = malloc(c * sizeof(uint32_t)), n = 0, *o = 0, g = 0;
     char* ln = 0;
     while (read_line(stdin, &ln, &l)) {
-        n++;
-        o = o ? o : calloc(l, sizeof(int32_t));
-        for (size_t i = 0; i < l; i++)
-            o[i] += (strtoul(ln, 0, 2) & 1 << i) > 0;
+        uint32_t b = strtoul(ln, 0, 2);
+        o = o || p ? o : calloc(l, sizeof(int32_t));
+        (a = ++n > c ? realloc(a, (c *= 2) * sizeof(uint32_t)) : a)[n - 1] = b;
+        for (size_t i = 0; i < (1 - p) * l; i++) o[i] += (b & 1 << i) > 0;
     }
-    for (size_t i = 0; i < l; i++)
-        g |= (o[i] > n / 2) * 1 << i;
-    free(ln);
-    free(o);
-    printf("%u\n", g * (~g & (1 << l) - 1));
-}
-
-void part2(void) {
-    size_t c = MAGIC_NUMBER, l = 0;
-    uint32_t *a = malloc(c * sizeof(uint32_t)), n = 0;
-    char* ln = 0;
-    while (read_line(stdin, &ln, &l)) (a = ++n > c ? realloc(a, (c *= 2) * sizeof(uint32_t)) : a)[n - 1] = strtoul(ln, NULL, 2);
     uint32_t *s, *e, r[2];
-    for (uint32_t b = 0; b < 2; b++) {
+    for (uint32_t b = 0; b < p * 2; b++) {
         e = (s = a) + n;
         for (size_t i = 0; i < l; i++) {
             evil_global = 1 << l - 1 - i;
             qsort(s, e - s, sizeof(uint32_t), cmpui32asc);
-            if (b ^ (s[(e - s) / 2] & evil_global) > 0) while (!(*s & evil_global)) s++; else while (*(e - 1) & evil_global) e--;
+            if (b ^ (s[(e - s) / 2] & evil_global) > 0)
+                while (!(*s & evil_global)) s++;
+            else
+                while (*(e - 1) & evil_global) e--;
             if (e - s == 1) { r[b] = *s; break; }
         }
     }
+    for (size_t i = 0; i < (1 - p) * l; i++) g |= (o[i] > n / 2) * 1 << i;
     free(ln);
     free(a);
-    printf("%u\n", r[0] * r[1]);
-}
-
-int main(int argc, char** argv) {
-    if (argc == 2 && !strcmp(argv[1], "1"))
-        part1();
-    else if (argc == 2 && !strcmp(argv[1], "2"))
-        part2();
-    else {
-        printf("Invalid arguments.");
-        return -1;
-    }
+    free(o);
+    printf("%u\n", (1 - p) * (g * (~g & (1 << l) - 1)) + p * r[0] * r[1]);
     return 0;
 }
