8d8bb4131e8e06deaac6ee7b2c5dba63722cc484
Kumodatsu <dragonkumo@gmail.com>
2021-12-03 20:44:40 +0100

    Add day 3 part 2 solution

--- src/day-03/Main.c
+++ src/day-03/Main.c
@@ -1,13 +1,20 @@
 #include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
 #include "Line.h"
 
-int main(void) {
+uint32_t evil_global = 0;
+
+int cmpui32asc(const void* a, const void* b) {
+    return (*(uint32_t*) a & evil_global) - (*(uint32_t*) b & evil_global);
+}
+
+void part1(void) {
     size_t   length = 0;
     uint32_t gamma  = 0;
     {
-        int32_t* ones = NULL;
-        uint32_t n    = 0;
+        uint32_t* ones = NULL;
+        uint32_t  n    = 0;
         {
             char* line = NULL;
             while (read_line(stdin, &line, &length)) {
@@ -28,5 +35,85 @@ int main(void) {
     uint32_t epsilon     = ~gamma & (1 << length) - 1;
     uint32_t consumption = gamma * epsilon;
     printf("%u\n", consumption);
+}
+
+void part2(void) {
+    size_t    bs_cap = 64;
+    uint32_t* bs     = malloc(bs_cap * sizeof(uint32_t));
+    size_t    length = 0;
+    uint32_t  n      = 0;
+
+    {
+        char* line = NULL;
+        while (read_line(stdin, &line, &length)) {
+            uint32_t b = strtoul(line, NULL, 2);
+            n++;
+            if (n > bs_cap) {
+                bs_cap *= 2;
+                bs = realloc(bs, bs_cap * sizeof(uint32_t));
+            }
+            bs[n - 1] = b;
+        }
+        free(line);
+    }
+
+    uint32_t* start;
+    uint32_t* end;
+    uint32_t  r_oxygen;
+    uint32_t  r_scrubber;
+
+    // oxygen generator rating
+    start = bs;
+    end   = bs + n;
+    for (size_t i = 0; i < length; i++) {
+        evil_global = 1 << length - 1 - i;
+        qsort(start, end - start, sizeof(uint32_t), cmpui32asc);
+        size_t   half = (end - start) / 2;
+        if (start[half] & evil_global) {
+            while (!(*start & evil_global))
+                start++;
+        } else {
+            while (*(end - 1) & evil_global)
+                end--;
+        }
+        if (end - start == 1) {
+            r_oxygen = *start;
+            break;
+        }
+    }
+
+    // CO2 scrubber rating
+    start = bs;
+    end   = bs + n;
+    for (size_t i = 0; i < length; i++) {
+        evil_global = 1 << length - 1 - i;
+        qsort(start, end - start, sizeof(uint32_t), cmpui32asc);
+        size_t half = (end - start) / 2;
+        if (start[half] & evil_global) {
+            while (*(end - 1) & evil_global)
+                end--;
+        } else {
+            while (!(*start & evil_global))
+                start++;
+        }
+        if (end - start == 1) {
+            r_scrubber = *start;
+            break;
+        }
+    }
+
+    free(bs);
+    printf("%u\n", r_oxygen * r_scrubber);
+}
+
+int main(int argc, char** argv) {
+    if (argc == 2 && !strcmp(argv[1], "1"))
+        part1();
+    else if (argc == 2 && !strcmp(argv[1], "2"))
+        part2();
+    else {
+        printf("Invalid arguments.");
+        return -1;
+    }
     return 0;
 }
---
+++ src/day-03/README.md
@@ -0,0 +1,10 @@
+# Day 3 - Binary Diagnostic with C
+You can find the description of the problem [here][1].
+
+## About the solution
+Since a lot of this problem can be solved using fancy bit manipulation, C seems
+like an excellent choice to solve it. Also in the spirit of C, the program is as
+unreadable as possible, left completely undocumented and may or may not produce
+the intended result with your choice of compiler and environment.
+
+[1]: <https://adventofcode.com/2021/day/3>
