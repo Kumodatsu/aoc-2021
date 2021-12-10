717379369b0dd736318ef1aa4f193405eeafb368
Kumodatsu <dragonkumo@gmail.com>
2021-12-10 20:54:11 +0100

    Add day 10 solution

---
+++ solutions/day-10/README.md
@@ -0,0 +1,16 @@
+# Day 10 - Syntax Scoring with C#
+You can find the description of the problem [here][1].
+
+## About the solution
+stack
+
+## How to run
+To run the solution, you need to have the .NET 6 SDK installed. Use the
+following command to run, where `<PART>` is either `1` or `2` for Part 1 and
+Part 2 respectively:
+
+    cat input/input.txt | dotnet run <PART>
+
+The solution is written to the standard output.
+
+[1]: <https://adventofcode.com/2021/day/10>
---
+++ solutions/day-10/src/Main.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+
+int p;
+if (args.Length != 1 || !int.TryParse(args[0], out p) || p is not (1 or 2)) {
+    System.Console.WriteLine("You must specify the part (either 1 or 2).");
+    return;
+}
+
+Dictionary<char, int>
+    syntax_values = new() {
+        { ')', 3     },
+        { ']', 57    },
+        { '}', 1197  },
+        { '>', 25137 }
+    },
+    completion_values = new() {
+        { '(', 1 },
+        { '[', 2 },
+        { '{', 3 },
+        { '<', 4 }
+    };
+
+Stack<char> stack             = new();
+List<Int64> completion_scores = new();
+int         syntax_score      = 0;
+string?     line              = null;
+
+int corrupted = 0;
+while ((line = Console.In.ReadLine()) is not null) {
+    foreach (char c in line) {
+        if (c is '(' or '[' or '{' or '<') {
+            stack.Push(c);
+            continue;
+        } else if ((stack.Pop(), c) is
+                ('(', ')') or ('[', ']') or ('{', '}') or ('<', '>')) {
+            continue;
+        } else {
+            syntax_score += syntax_values[c];
+            stack.Clear();
+            corrupted++;
+            break;
+        }
+    }
+    if (stack.Count != 0) {
+        Int64 completion_score = 0;
+        while (stack.TryPop(out char c))
+            completion_score = completion_score * 5 + completion_values[c];
+        completion_scores.Add(completion_score);        
+    }
+}
+
+switch (p) {
+    case 1:
+        Console.Out.WriteLine(syntax_score);
+        break;
+    case 2:
+        completion_scores.Sort();
+        Console.Out.WriteLine(completion_scores[completion_scores.Count / 2]);
+        break;
+}
